675568d52274453d79ac8b37e19eadcd408a872f
unknown <gregmasterbr@gmail.com>
2022-01-25 17:02:25 -0300

    first commit - e criado o primeiro app - de autenticação.

---
+++ imobi/autenticacao/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+#from django.contrib.auth.models import User
+
+# Create your models here.
+# class User():
+#     name = models.CharField(max_length=200, null=True)
+#     email = models.EmailField(unique=True, null=True)
+#     senha = models.TextField(null=True, blank=True)
+#     updated = models.DateTimeField(auto_now=True)
+#     created = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         ordering = ['-updated', '-created']   
+
+#     def __str__(self):
+#         return self.name
