017d71cc5a3a083aec187fc0952839727df56865
unknown <gregmasterbr@gmail.com>
2022-01-27 08:28:44 -0300

    Adicionado funcionalidades para solicitar agendamento baseado nas informações de cadastro dos imoveis, uma tela para visualizar os agendamentos e a possibilidade de cancelar um agendamento

--- imobi/plataforma/views.py
+++ imobi/plataforma/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import redirect, render,get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Cidade, Imovel
+from .models import Cidade, Imovel, Visitas
 
 # Create your views here.
 
@@ -35,3 +35,29 @@ def imovel(request, id):
     imovel = get_object_or_404(Imovel, id=id)
     sugestoes = Imovel.objects.filter(cidade=imovel.cidade).exclude(id=id)[:2]
     return render(request, 'imovel.html', {'imovel': imovel, 'sugestoes': sugestoes, 'id': id})
+
+def agendar_visitas(request):
+    usuario = request.user
+    dia = request.POST.get('dia')
+    horario = request.POST.get('horario')
+    id_imovel = request.POST.get('id_imovel')
+
+    visita = Visitas(
+        imovel_id=id_imovel,
+        usuario=usuario,
+        dia=dia,
+        horario=horario
+    )
+    visita.save()
+
+    return redirect('/agendamentos')
+
+def agendamentos(request):
+    visitas = Visitas.objects.filter(usuario=request.user)
+    return render(request, "agendamentos.html", {'visitas': visitas})
+
+def cancelar_agendamento(request, id):
+    visitas = get_object_or_404(Visitas, id=id)
+    visitas.status = "C"
+    visitas.save()
+    return redirect('/agendamentos')    
